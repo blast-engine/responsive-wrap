d7c524f81330ab3b8b5ced5e0f848d55fd4b93cc
armin ghobadi <armin.ghobadi95@gmail.com>
2020-04-15 17:56:29 -0400

    d

--- src/index.js
+++ src/index.js
@@ -1,6 +1,49 @@
 import React from 'react'
-import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+import './styles.module.css'
+
+export const ExampleComponent = class ResponsiveGrid extends React.Component {
+  
+  state = { numCols: 1 }
+
+  containerRef = React.createRef()
+
+  componentDidMount() {
+    this.calculateNumColsPerRow()
+    window.addEventListener('resize', () => this.calculateNumColsPerRow())
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize')
+  }
+
+  calculateNumColsPerRow() {
+    const { minWidth, children } = this.props
+    const numCols = Math.floor(this.containerRef.current.offsetWidth / minWidth)
+    this.setState({ numCols: Math.min(numCols, children.length) })
+  }
+
+  render() {
+    const { maxWidth, children, outerDivClassName } = this.props
+    const { numCols } = this.state
+
+    return (
+      <div ref={this.containerRef} className='inner-div'>
+        {
+          children.map((child, index) => (
+            <div 
+              style={{ 
+                width: 100/numCols + '%',
+                maxWidth
+              }} 
+              className={outerDivClassName || ''}
+              key={child.key || index}
+            >
+              {child}
+            </div>
+          ))
+        }
+      </div>
+    )
+  }
 }
